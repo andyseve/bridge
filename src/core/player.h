0bcc278b2540c085d5d98a4bc6df750b5c2c0286
Anish Sevekari <anish.sevekari@gmail.com>
2019-05-02 18:37:34 -0400

    creating player class

---
+++ src/core/player.h
@@ -0,0 +1,12 @@
+ /* Author:Anish Sevekari
+  * Last Modified: Thu 02 May 2019 06:37:07 PM EDT
+  * Provides class player */
+
+namespace bridge {
+	class player {
+		public:
+			string name;
+			int games;
+
+	}
+}
