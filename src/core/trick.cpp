6a454ac24385f40f0a91e864540a30edd0a9be72
Anish Sevekari <anish.sevekari@gmail.com>
2019-05-14 13:39:43 -0400

    sync

--- src/core/trick.cpp
+++ src/core/trick.cpp
@@ -1,9 +1,17 @@
 /* Author: Anish Sevekari
- * Last Modified: Tue 14 May 2019 10:38:09 AM EDT
+ * Last Modified: Tue 14 May 2019 01:39:59 PM EDT
  * Function for trick class */
 
 #include "cstdio"
 #include "cstdlib"
 #include "iostream"
 
+#include "trick.h"
+
+namespace bridge {
+	void trick::set_cards(std::string input){
+		for(uint i=0,j=0; i<4 && j < input.size();j++){
+		}
+	}
+}
 
